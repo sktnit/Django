6209249ac5a8df8cdf879955993c1d0efa8745ae
shaileshnit <sktmanit@gmail.com>
2020-11-08 03:53:37 +0530

    Update word counter and character counter

--- textutility/textutility/settings.py
+++ textutility/textutility/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ei7^k1@xo+&==0zo&j4t+*62^5&7or^y-d_b1a0_29wroy$%rl'
+SECRET_KEY = 'ei7^k1@xo+&==0zo&j4t+*62^5&7or^y-d_b1a0_29wroy$%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
